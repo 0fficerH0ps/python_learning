883b6be5d0eea1902df4fd640d793d99188c4636
0fficerH0ps <hannahbrd15@gmail.com>
2022-03-02 23:35:43 -0600

    More Chalanges!

---
+++ chapter_8/message.py
@@ -0,0 +1,8 @@
+# 8-1. Message: Write a function called display_message() that prints one sentence
+# telling everyone what you are learning about in this chapter. Call the
+# function, and make sure the message displays correctly.
+
+def display_message():
+    """Display a message about chapter 8."""
+    print("I am learning about functions in chapter 8.")
+display_message()
