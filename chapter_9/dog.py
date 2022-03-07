315a8842c9735f971b548a1ecb382226319a887b
0fficerH0ps <hannahbrd15@gmail.com>
2022-03-07 14:25:44 -0600

    Learning

--- chapter_9/dog.py
+++ chapter_9/dog.py
@@ -84,3 +84,26 @@ my_dog.roll_over()
 
 # Willie is now sitting.
 # Willie rolled over!
+
+
+# Creating Multiple Instances
+
+# class Dog:
+#     --snip--
+
+my_dog = Dog('Willie', 6)
+your_dog = Dog('Lucy', 3)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
+
+# Even if we used the same name and age for the second dog, Python
+# would still create a separate instance from the Dog class. You can make
+# as many instances from one class as you need, as long as you give each
+# instance a unique variable name or it occupies a unique spot in a list or
+# dictionary.
